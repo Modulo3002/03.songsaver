7b5d319a4582166156c0d055f0d9ceff0320e7ab
Robin <81294929+Modulo3002@users.noreply.github.com>
2023-04-04 15:41:36 +0200

    inputboxes gemaakt

---
+++ src/components/Inputboxes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Inputbox() {
+  return (
+    <form>
+      <input type="text" name="inputbox" placeholder="dit is een box"></input>
+    </form>
+  );
+}
+
+export default Inputbox;
