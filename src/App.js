53a786c51cc7a203bc8df5e0419ca89380e0a947
Robin <81294929+Modulo3002@users.noreply.github.com>
2023-04-05 13:31:10 +0200

    dropdown component gebouwd dropdown toegevoegd vergeten losse branch te maken :(

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Inputbox from "./components/Inputboxes";
+import Dropdownmenu from "./components/Dropdown";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         </a>
       </header>
       <Inputbox />
+      <Dropdownmenu />
     </div>
   );
 }
---
+++ src/components/Dropdown.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Dropdownmenu() {
+  return (
+    <select>
+      <option value="">Kies een muziekgenre</option>
+      <option value="pop">Pop</option>
+      <option value="rock">Rock</option>
+      <option value="dance">Dance</option>
+      <option value="disco">Disco</option>
+      <option value="electronic">electronic</option>
+      <option value="hip-hop">Hip-Hop</option>
+      <option value="ballad">Ballad</option>
+    </select>
+  );
+}
+
+export default Dropdownmenu;
